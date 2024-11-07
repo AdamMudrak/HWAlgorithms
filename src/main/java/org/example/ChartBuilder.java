4236ab9a809d490fc5f8b506063699c744186e9e
Adam Mudrak <mudrets2000@gmail.com>
2024-11-07 22:22:18 +0200

    Add frequency builder

--- src/main/java/org/example/ChartBuilder.java
+++ src/main/java/org/example/ChartBuilder.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import java.util.List;
+import java.util.Map;
 import javax.swing.JFrame;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
@@ -35,6 +37,26 @@ public class ChartBuilder {
         chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
 
         JFrame frame = new JFrame(chartTitle);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(chartPanel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void buildHistograms(Map<Integer, Long> frequencyMap) {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        frequencyMap.forEach((key, value) -> {
+            dataset.addValue(value, "Frequency", key.toString());
+        });
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Frequency Chart",
+                "Value",
+                "Frequency",
+                dataset
+        );
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(chartPanel);
         frame.pack();
