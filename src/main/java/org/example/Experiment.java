8ded4557a61e0d3408ccc2925deb4aa10603bae1
Adam Mudrak <mudrets2000@gmail.com>
2024-11-07 13:40:42 +0200

    Have only one experiment class

--- src/main/java/org/example/Experiment.java
+++ src/main/java/org/example/Experiment.java
@@ -1,18 +1,19 @@
-package org.example.hillclimbing;
+package org.example;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class HCExperiment {
+public class Experiment {
 
     public void analyzeResults(List<Integer> solutions) {
         int bestSolution = Collections.min(solutions);
         int worstSolution = Collections.max(solutions);
         System.out.println("Краще рішення: " + bestSolution);
         System.out.println("Гірше рішення: " + worstSolution);
-        System.out.println("Різниця між кращим та гіршим рішенням: " + (worstSolution - bestSolution));
+        System.out.println("Різниця між кращим та гіршим рішенням: "
+                + (worstSolution - bestSolution));
         System.out.println("Гістограми рішень:");
         printHistograms(solutions);
         double averageSolution = solutions.stream().mapToInt(Integer::intValue).average().orElse(0);
@@ -22,21 +23,22 @@ public class HCExperiment {
     }
 
     private void printHistograms(List<Integer> solutions) {
-        Map<Integer, Long> frequencyMap = solutions.stream()
-                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
         System.out.print("   0%");
         int startPercent = 10;
         System.out.print(" ".repeat(5) + startPercent + "%");
-        for(int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             startPercent += 10;
             System.out.print(" ".repeat(7) + startPercent + "%");
             if (i == 8) {
                 System.out.println();
             }
         }
+        Map<Integer, Long> frequencyMap = solutions.stream()
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
         System.out.println("   " + "#".repeat(100));
         frequencyMap.forEach((solution, frequency) ->
-                System.out.println(" ".repeat(Math.toIntExact(frequency)) + frequency + "%" + System.lineSeparator()
+                System.out.println(" ".repeat(Math.toIntExact(frequency)) + frequency + "%"
+                        + System.lineSeparator()
                         + solution + ": " + "#".repeat(Math.toIntExact(frequency))));
     }
 }
