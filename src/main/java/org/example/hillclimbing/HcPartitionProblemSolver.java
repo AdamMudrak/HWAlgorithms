39fa8f192c3d17d21bf361290765dd741f3b89b7
Adam Mudrak <mudrets2000@gmail.com>
2024-11-07 13:41:15 +0200

    Finalize code style of HcPartitionProblemSolver

--- src/main/java/org/example/hillclimbing/HcPartitionProblemSolver.java
+++ src/main/java/org/example/hillclimbing/HcPartitionProblemSolver.java
@@ -1,44 +1,65 @@
 package org.example.hillclimbing;
 
-import org.example.CommonFunctionsUtil;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import org.example.util.CommonFunctionsUtil;
+import org.example.util.PrinterUtil;
 
-public class HCPartitionProblemSolver {
+public class HcPartitionProblemSolver {
     private final int[] set;
     private final Random random = new Random();
+    private int solutionCounter;
 
-    public HCPartitionProblemSolver(int[] set) {
+    public HcPartitionProblemSolver(int[] set) {
         this.set = set;
     }
 
     public List<Integer> multiStartHillClimb(int numStarts) {
+        solutionCounter = 1;
         List<Integer> solutions = new ArrayList<>();
         for (int i = 0; i < numStarts; i++) {
             int result = hillClimb();
             solutions.add(result);
+            solutionCounter++;
         }
         return solutions;
     }
 
     private int hillClimb() {
+        PrinterUtil.printSpliterator();
+
         boolean[] partition = CommonFunctionsUtil.generateRandomPartition(set, random);
+
         int currentDifference = CommonFunctionsUtil.calculateDifference(set, partition);
+        PrinterUtil.printInitialDifference(currentDifference, solutionCounter);
+
+        boolean notImproved = true;
+
         while (true) {
             boolean improved = false;
             for (int i = 0; i < set.length; i++) {
+
                 partition[i] = !partition[i];
+
                 int newDifference = CommonFunctionsUtil.calculateDifference(set, partition);
+
                 if (newDifference < currentDifference) {
                     currentDifference = newDifference;
+                    PrinterUtil.printImprovedDifference(currentDifference, solutionCounter);
+
+                    notImproved = false;
                     improved = true;
                 } else {
                     partition[i] = !partition[i];
                 }
             }
-            if (!improved) break;
+            if (!improved) {
+                break;
+            }
+        }
+        if (notImproved) {
+            PrinterUtil.notImprovedPrinter(solutionCounter);
         }
         return currentDifference;
     }
