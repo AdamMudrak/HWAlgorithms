1f2e999e4b9ffc31cdcab8b630515a16feebe183
Adam Mudrak <mudrets2000@gmail.com>
2024-11-07 16:47:42 +0200

    Add DB imitation for hillClimb

---
+++ src/main/java/org/example/HcIterationDb.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HcIterationDb {
+    public static final List<Integer> hillClimbIterationSolutions = new ArrayList<>();
+}
