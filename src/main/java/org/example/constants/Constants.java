04e034bd80f1267a1898fccdd775cef3b90b74e4
Adam Mudrak <mudrets2000@gmail.com>
2024-11-07 14:51:18 +0200

    Delete unused constants and change how many times SA is called

--- src/main/java/org/example/constants/Constants.java
+++ src/main/java/org/example/constants/Constants.java
@@ -2,11 +2,9 @@ package org.example.constants;
 
 public class Constants {
     public static final int HC_ALGORITHMS_CALLS = 100;
-    public static final int SA_ALGORITHMS_CALLS = 100;
+    public static final int SA_ALGORITHMS_CALLS = 1;
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
 
     public static final String SPLITERATOR = "-";
