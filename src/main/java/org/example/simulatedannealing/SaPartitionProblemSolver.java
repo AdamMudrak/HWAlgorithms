9fdeaaaacc888cdf8c096a62ef627c5939173d85
Adam Mudrak <mudrets2000@gmail.com>
2024-11-07 13:45:43 +0200

    Save codestyle improvements

--- src/main/java/org/example/simulatedannealing/SaPartitionProblemSolver.java
+++ src/main/java/org/example/simulatedannealing/SaPartitionProblemSolver.java
@@ -1,31 +1,38 @@
 package org.example.simulatedannealing;
 
-import org.example.CommonFunctionsUtil;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import org.example.util.CommonFunctionsUtil;
+import org.example.util.PrinterUtil;
 
-public class SAPartitionProblemSolver {
+public class SaPartitionProblemSolver {
     private final int[] set;
     private final Random random = new Random();
+    private int solutionCounter;
 
-    public SAPartitionProblemSolver(int[] set) {
+    public SaPartitionProblemSolver(int[] set) {
         this.set = set;
     }
 
     public List<Integer> multiStartSimulatedAnnealing(int numStarts) {
+        solutionCounter = 1;
         List<Integer> solutions = new ArrayList<>();
         for (int i = 0; i < numStarts; i++) {
             int result = simulatedAnnealing();
             solutions.add(result);
+            solutionCounter++;
         }
         return solutions;
     }
 
     private int simulatedAnnealing() {
+        PrinterUtil.printSpliterator();
+
         boolean[] partition = CommonFunctionsUtil.generateRandomPartition(set, random);
+
         int currentDifference = CommonFunctionsUtil.calculateDifference(set, partition);
+
         double temperature = 10000;
         double coolingRate = 0.003;
 
@@ -47,15 +54,16 @@ public class SAPartitionProblemSolver {
     private boolean[] generateNeighborPartition(boolean[] partition) {
         boolean[] newPartition = partition.clone();
         int index = random.nextInt(set.length);
-        newPartition[index] = !newPartition[index];  // Toggle the element's set
+        newPartition[index] = !newPartition[index];
         return newPartition;
     }
 
-    private boolean shouldAcceptSolution(int currentDifference, int newDifference, double temperature) {
+    private boolean shouldAcceptSolution(int currentDifference, int newDifference,
+                                         double temperature) {
         if (newDifference < currentDifference) {
-            return true;  // Always accept better solutions
+            return true;
         }
         double acceptanceProbability = Math.exp((currentDifference - newDifference) / temperature);
-        return random.nextDouble() < acceptanceProbability;  // Accept worse solutions with a certain probability
+        return random.nextDouble() < acceptanceProbability;
     }
 }
