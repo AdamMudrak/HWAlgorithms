503abd7f5a6ce2696ef6f18edc284626d6ea7528
Adam Mudrak <mudrets2000@gmail.com>
2024-11-07 22:22:31 +0200

    Delete unused constants

--- src/main/java/org/example/constants/Constants.java
+++ src/main/java/org/example/constants/Constants.java
@@ -3,7 +3,4 @@ package org.example.constants;
 public class Constants {
     public static final int HC_ALGORITHMS_CALLS = 100;
     public static final int SA_ALGORITHMS_CALLS = 1;
-
-    public static final String SPLITERATOR = "-";
-    public static final int REPEAT_SPLITERATOR = 120;
 }
