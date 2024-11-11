42c9792d358545f8e9b7347c9eb33ca584496996
Adam Mudrak <mudrets2000@gmail.com>
2024-11-11 19:07:22 +0200

    Build ranged chart

--- src/main/java/org/example/util/ChartBuilderUtil.java
+++ src/main/java/org/example/util/ChartBuilderUtil.java
@@ -1,5 +1,6 @@
 package org.example.util;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import javax.swing.JFrame;
@@ -45,7 +46,33 @@ public class ChartBuilderUtil {
 
     public static void buildHistograms(Map<Double, Long> frequencyMap) {
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        frequencyMap.forEach((key, value) -> dataset.addValue(value, "Frequency", key.toString()));
+        Map<Double, Long> frequencyMapRanged = new LinkedHashMap<>();
+        fillFrequencyMapRanged(frequencyMapRanged);
+        for (Map.Entry<Double, Long> entry : frequencyMap.entrySet()) {
+            if (entry.getKey() < 0.1) {
+                frequencyMapRanged.put(0.1, frequencyMapRanged.get(0.1) + entry.getValue());
+            } else if (entry.getKey() < 0.2) {
+                frequencyMapRanged.put(0.2, frequencyMapRanged.get(0.2) + entry.getValue());
+            } else if (entry.getKey() < 0.3) {
+                frequencyMapRanged.put(0.3, frequencyMapRanged.get(0.3) + entry.getValue());
+            } else if (entry.getKey() < 0.4) {
+                frequencyMapRanged.put(0.4, frequencyMapRanged.get(0.4) + entry.getValue());
+            } else if (entry.getKey() < 0.5) {
+                frequencyMapRanged.put(0.5, frequencyMapRanged.get(0.5) + entry.getValue());
+            } else if (entry.getKey() < 0.6) {
+                frequencyMapRanged.put(0.6, frequencyMapRanged.get(0.6) + entry.getValue());
+            } else if (entry.getKey() < 0.7) {
+                frequencyMapRanged.put(0.7, frequencyMapRanged.get(0.7) + entry.getValue());
+            } else if (entry.getKey() < 0.8) {
+                frequencyMapRanged.put(0.8, frequencyMapRanged.get(0.8) + entry.getValue());
+            } else if (entry.getKey() < 0.9) {
+                frequencyMapRanged.put(0.9, frequencyMapRanged.get(0.9) + entry.getValue());
+            } else if (entry.getKey() < 1.0) {
+                frequencyMapRanged.put(1.0, frequencyMapRanged.get(1.0) + entry.getValue());
+            }
+        }
+        frequencyMapRanged.forEach((key, value) -> dataset.addValue(value,
+                "Frequency", key.toString()));
         JFreeChart chart = ChartFactory.createBarChart(
                 "Frequency Chart",
                 "Value",
@@ -59,5 +86,18 @@ public class ChartBuilderUtil {
         frame.getContentPane().add(chartPanel);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    private static void fillFrequencyMapRanged(Map<Double, Long> frequencyMapRanged) {
+        frequencyMapRanged.put(0.1, 0L);
+        frequencyMapRanged.put(0.2, 0L);
+        frequencyMapRanged.put(0.3, 0L);
+        frequencyMapRanged.put(0.4, 0L);
+        frequencyMapRanged.put(0.5, 0L);
+        frequencyMapRanged.put(0.6, 0L);
+        frequencyMapRanged.put(0.7, 0L);
+        frequencyMapRanged.put(0.8, 0L);
+        frequencyMapRanged.put(0.9, 0L);
+        frequencyMapRanged.put(1.0, 0L);
     }
 }
