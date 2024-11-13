575fff50d0775fa218a20d263809da8df6a5ce6a
Adam Mudrak <mudrets2000@gmail.com>
2024-11-13 11:05:43 +0200

    Modify main according to project level changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,38 @@
 package org.example;
 
-import static org.example.hillclimbing.Db.VALUES;
-import static org.example.hillclimbing.Db.hillClimbIterationSolutions;
+import static org.example.hillclimbing.Db.HARD_VALUES;
+import static org.example.hillclimbing.Db.SIMPLE_VALUES;
 
 import java.util.List;
 import java.util.stream.Collectors;
 import org.example.hillclimbing.HcPartitionProblemSolver;
-import org.example.simulatedannealing.SaPartitionProblemSolver;
+import org.example.simulatedannealing.SumPartitionSimulatedAnnealing;
 import org.example.util.ChartBuilderUtil;
 import org.example.util.StatisticsUtil;
 
 public class Main {
 
     public static void main(String[] args) {
-        hcExperimentRunner();
+        //hcExperimentRunner();
         saExperimentRunner();
     }
 
     private static void hcExperimentRunner() {
-        HcPartitionProblemSolver solver = new HcPartitionProblemSolver(VALUES);
+        HcPartitionProblemSolver solver = new HcPartitionProblemSolver(HARD_VALUES);
 
         StatisticsUtil hcExperiment = new StatisticsUtil();
 
         List<Double> solutions = solver.multiStartHillClimb(100);
-        ChartBuilderUtil.build(hillClimbIterationSolutions,"Графік збіжності Hill Climb");
+        ChartBuilderUtil.build(solver.getHillClimbIterationSolutions(),"Графік збіжності Hill Climb");
         ChartBuilderUtil.buildHistograms(solutions.stream()
                 .collect(Collectors.groupingBy(e -> e, Collectors.counting())));
         hcExperiment.analyzeResults(solutions);
     }
 
     private static void saExperimentRunner() {
-        SaPartitionProblemSolver solver = new SaPartitionProblemSolver(VALUES);
-        solver.simulatedAnnealing();
-        ChartBuilderUtil.build(solver.getIterationSolutions(),
+        SumPartitionSimulatedAnnealing simulatedAnnealing = new SumPartitionSimulatedAnnealing(HARD_VALUES);
+        System.out.println("Краще: " + simulatedAnnealing.simulatedAnnealing());
+        ChartBuilderUtil.build(simulatedAnnealing.getSolutionCollector(),
                 "Графік збіжності Simulated Annealing");
     }
 }
